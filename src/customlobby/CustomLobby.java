f0ee3c84a4276ecdab34240f6d8685e5bc9d7844
MayusYT <mariusschuh@outlook.de>
2018-08-05 22:43:52 +0200

    plugin.yml

--- src/customlobby/CustomLobby.java
+++ src/customlobby/CustomLobby.java
@@ -1,1 +1,33 @@
-//Hi.
+
+package customlobby;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CustomLobby extends JavaPlugin {
+    public static CustomLobby instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        initConfig();
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    private void initConfig() {
+        saveConfig();
+        reloadConfig();
+        getConfig().addDefault("player.DEFAULT.exist", false);
+        getConfig().addDefault("spawn.DEFAULT.exist", false);
+        saveDefaultConfig();
+        saveConfig();
+        reloadConfig();
+    }
+
+    public static CustomLobby getInstance() {
+        return instance;
+    }
+}
